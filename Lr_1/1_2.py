82afccc710189b07648a739ed438828718eaa09e
Mrakooo <100166631+Mrakooo@users.noreply.github.com>
2022-03-28 16:08:30 +0300

    Lr_1

---
+++ Lr_1/1_2.py
@@ -0,0 +1,15 @@
+"""
+Марченко Андрій 141
+16. Визначити число, яке отримано записом у зворотному порядку цифр заданого тризначного
+числа.
+"""
+def reverse(num):
+    a = num // 100
+    b = num % 100 // 10 * 10
+    c = num % 10 * 100
+    num = a + b + c
+    print(num)
+    input()
+
+num=int(input("Enter a three-digit number:"))
+reverse(num)
---
+++ Lr_1/1_4.py
@@ -0,0 +1,22 @@
+"""
+Марченко Андрій 141
+6. Поміняти місцями вміст змінних A і B і вивести нові значення A і B.
+"""
+
+def reverse_a_b():
+    A = "True"
+    B = 141
+    C = None
+    print("A=" + str(A))
+    print("B=" + str(B))
+
+    print("----------")
+
+    C = A
+    A = B
+    B = C
+    print("A=" + str(A))
+    print("B=" + str(B))
+    input()
+
+reverse_a_b()
